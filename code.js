7ae45c784756146dbe7f34970ecd4c50a9854e59
MrBitz <76222941+MrBitz@users.noreply.github.com>
2021-03-08 12:26:25 +0400

    Check code

---
+++ code.js
@@ -0,0 +1,7 @@
+function clicked(){
+  var code = document.getElementById("Code").value;
+  String(code)
+  if (code === "0001"){
+    document.getElementById("info").innerHTML = "This is the beta error. Since the program isn't completely made and tested it has errors.";
+  }
+}
