ce0ee091831bc93f0980a41ea1f5627835e099c9
MrBitz <76222941+MrBitz@users.noreply.github.com>
2022-02-07 12:29:23 +0400

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function clicked(){
   var code = document.getElementById("Code").value;
   String(code)
   if (code === "0001"){
-    document.getElementById("info").innerHTML = "You have changed variable values! Reset your console and report this if you haven't changed them, it could be a virus.";
+    document.getElementById("info").innerHTML = "You have changed variable values! Reset your console and report this if you haven't changed them.";
   } else {
     document.getElementById("info").innerHTML = "Please enter a valid code.";
   }
