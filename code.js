da342cba2e7985242268dfc2ae272d74c2a07831
MrBitz <76222941+MrBitz@users.noreply.github.com>
2022-02-07 18:43:08 +0400

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
 function clicked(){
-  const codelist = ["0001", "etd"];
+  codelist = ["0001", "etd"];
   var code = document.getElementById("Code").value;
   String(code)
+  inlist = (code in codelist)
   if (code === "0001"){
     document.getElementById("info").innerHTML = "You have changed variable values! Reset your console and report this if you haven't changed them.";
   }
   if (code === "etd"){
     window.open("https://mrbitz.github.io/etd.html", "_self");
   }
-  if !(code in codelist){
+  if (inlist == false){
     document.getElementById("info").innerHTML = "Please enter a valid code.";
   }
 }
