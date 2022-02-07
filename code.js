4366ad58e00fb19d2f907ac9e6a72c80bf384a31
MrBitz <76222941+MrBitz@users.noreply.github.com>
2022-02-07 18:03:01 +0400

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function clicked(){
   String(code)
   if (code === "0001"){
     document.getElementById("info").innerHTML = "You have changed variable values! Reset your console and report this if you haven't changed them.";
-  if (code === "etd"){
+  } else if (code === "etd"){
     document.getElementById("etd").innerHTML = "<a href='etd.html'>Escape The Dungeon Update list!</a>";
   } else {
     document.getElementById("info").innerHTML = "Please enter a valid code.";
